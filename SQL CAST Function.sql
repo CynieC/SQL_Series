36da93fae33c1b45615b10a89d263d5a41efaf9a
Cynthia Nafula <100769722+CynieC@users.noreply.github.com>
2023-07-10 15:07:12 +0300

    Add files via upload

---
+++ SQL CAST Function.sql
@@ -0,0 +1,16 @@
+
+
+SELECT * FROM Sales.SalesTaxRate
+
+SELECT ModifiedDate, CAST(ModifiedDate AS varchar(20))
+FROM Sales.SalesTaxRate
+
+/* PERCENTAGE CHANGE
+(NEW NUMBER-OLD NUMBER)/OLD NUMBER*/
+
+
+
+
+
+
+
